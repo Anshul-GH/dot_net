fe74fae600064a6dfbe93e2da31a3a29f5df9f20
Anshul-GH <samgalacestyle01@gmail.com>
2022-12-01 15:26:03 +0530

    Routine Commit

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.ReadLine();
+
+int harry = 34;
+
+Console.WriteLine("Printing sample number: " + harry)
